c067ed5ec1509d6ad8d671c854e158f66977318a
Daniel Lagos <daniel.lagos010@gmail.com>
2021-01-31 00:32:15 -0500

    create login with mongo

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -1,13 +1,32 @@
 const {response} = require('express');
 const jwt = require('jsonwebtoken');
 
-const validateJWT = (req, res = response) => {
+const validateJWT = (req, res = response, next) => {
     const token = req.header('x-token');
     if (!token) {
         return res.status(400).json({
             ok: false,
             msg: 'No hay token en la peticion'
         });
+    }
+    try {
+
+        const {nickName, name} = jwt.verify(
+            token,
+            process.env.SECRET_JWT_SEED
+        );
+
+        req.nickName = nickName;
+        req.name = name;
+
+        next();
+
+    } catch (e) {
+        console.log(e);
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no valido'
+        })
     }
 
 }
--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const UserSchema = Schema({
     },
     photo: {
         type: String,
-        required: true
+        required: false
     },
     channels: {
         type: [],
